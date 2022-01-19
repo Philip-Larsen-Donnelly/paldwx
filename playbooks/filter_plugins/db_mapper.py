5f089a97547f69e01bacacea85af8cfab343b6e3
Philip-Larsen-Donnelly <phil@dhis2.org>
2022-01-19 12:18:48 +0100

    feat: custom filter for db demo

--- playbooks/filter_plugins/db_mapper.py
+++ playbooks/filter_plugins/db_mapper.py
@@ -5,26 +5,45 @@ class FilterModule(object):
     def filters(self):
         return {'war_to_db': self.war_to_db}
 
-    def war_to_db(self, warfile):
-        
+
+    def war_to_db(self, warfile, dmap):
+
         pre_db = 'https://databases.dhis2.org/sierra-leone/'
         post_db = '/dhis2-db-sierra-leone.sql.gz'
 
         db_file = ""
-        if re.search("2\.[0-9][0-9]\.[0-9]+", warfile):
-            if re.search("2\.[0-9][0-9]\.[0-9]+-rc", warfile):
-                ver = re.search("2\.[0-9][0-9]\.[0-9]+", warfile).group(0)
-                patch = ver.split('.')[-1]
-                if int(patch) > 0:
-                    patch = int(patch) - 1
-                db_file = pre_db+ver[:4]+'.'+str(patch)+post_db
-            else:
-                ver = re.search("2\.[0-9][0-9]\.[0-9]+", warfile).group(0)
-                db_file = pre_db+ver+post_db
+
+        if warfile in dmap and dmap[warfile] != '':
+            db_file = dmap[warfile]
         else:
-            if re.search("2\.[0-9][0-9]", warfile):
-                ver = re.search("2\.[0-9][0-9]", warfile).group(0)
+            if re.search("2\.[0-9][0-9]\.[0-9]+", warfile):
+                if re.search("2\.[0-9][0-9]\.[0-9]+-rc", warfile):
+                    ver = re.search("2\.[0-9][0-9]\.[0-9]+", warfile).group(0)
+                    patch = ver.split('.')[-1]
+                    if int(patch) > 0:
+                        patch = int(patch) - 1
+                    db_file = pre_db+ver[:4]+'.'+str(patch)+post_db
+                else:
+                    ver = re.search("2\.[0-9][0-9]\.[0-9]+", warfile).group(0)
+                    db_file = pre_db+ver+post_db
             else:
-                ver = "dev"
-            db_file = pre_db+ver+post_db
+                if re.search("2\.[0-9][0-9]", warfile):
+                    ver = re.search("2\.[0-9][0-9]", warfile).group(0)
+                else:
+                    ver = "dev"
+                db_file = pre_db+ver+post_db
         return db_file
+
+    def get_war_link(self, reference):
+
+        prefix = 'https://releases.dhis2.org/'
+
+        # if the reference starts with `http` assume it is full link and return unchanged
+
+        # else if the reference ends in `.war` assume it is full path and append to prefix
+
+        # else if the reference contains full patch `2.xx.y` then
+            # generate the stable link (keep any prefix like `-rc` or `-EMBARGOED`)
+
+        # else determine if it is dev or canary
+        
